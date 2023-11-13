49e33c30eb9b84a9c6d29118347c3176a4c67a96
Nikola Marinov <102319114+NickolaM88@users.noreply.github.com>
2023-11-13 21:14:11 +0200

    Add files via upload

---
+++ Creatures/routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController');
+const creatureController = require('./controllers/creatureController');
+
+router.use(homeController);
+router.use(authController);
+router.use('/creature', creatureController);
+router.all('*', (req, res) => {
+    res.render('home/404');
+})
+
+module.exports = router;
